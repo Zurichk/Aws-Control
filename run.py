4cb321b9345f2092fb3a28798f3c758e1d46a146
Zurichk <zurich85aep@gmail.com>
2025-12-15 10:39:32 +0100

    Cambio puerto al 5041

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ from app.app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('FLASK_PORT', 5040))
+    port = int(os.environ.get('FLASK_PORT', 5041))
     app.run(host='0.0.0.0', port=port, debug=True)
